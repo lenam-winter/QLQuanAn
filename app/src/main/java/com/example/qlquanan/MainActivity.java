4b9648f99f2b64eec9ff93472e32988a2f8946d8
Huyle2003 <ledanghuy2003.dlieya@gmail.com>
2024-11-10 00:00:46 +0700

    this 20 21 22

--- app/src/main/java/com/example/qlquanan/MainActivity.java
+++ app/src/main/java/com/example/qlquanan/MainActivity.java
@@ -17,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String test="thu nha";
         String test1="thu nhaaa";
+        String testhuy="thu nhaaa1";
+        String testvuong="thu nhaaa2";
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
